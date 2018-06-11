de252d8c2e6361d7bae449053b8102ab67942a7e
MichaelZ <mzimdars@gms-online.de>
2018-06-11 16:49:45 +0200

    add patch from wt-libraries / xamarin for gesture view

---
+++ wt.core.xamarin/Classes/Forms/GestureView.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace WhileTrue.Classes.Forms
+{
+    public class GestureView : ContentView
+    {
+        public static readonly BindableProperty IsPressedProperty = BindableProperty.Create(@"IsPressed", typeof(bool), typeof(GestureView), false, BindingMode.TwoWay);
+
+        public bool IsPressed
+        {
+            set => this.SetValue(GestureView.IsPressedProperty, value);
+            get => (bool) this.GetValue(GestureView.IsPressedProperty);
+        }
+    }
+}
